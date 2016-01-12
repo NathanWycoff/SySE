6a0351b163187b66e271a605c934807b9d4212bb
Nathan Wycoff <nathanbrwycoff@gmail.com>
2016-01-12 12:32:02 +0100

    Added setup.py, etc. for distribution

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='syse',
+    version='1.0',
+    description='A syntactic sentence extraction program.',
+    url='https://github.com/NathanWycoff/SySE',
+    author='Nathan Wycoff',
+    author_email='nathanbrwycoff@gmail.com',
+    packages=['syse'],
+    install_requires = [
+        'pandas',
+        're',
+        'numpy',
+        'unicodedata',
+        'math'],
+    dependency_links = ['https://github.com/emilmont/pyStatParser/tarball/master'])
