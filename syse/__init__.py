870d76b7e6a492d8973de62fa670fae19699d240
Nathan Wycoff <nathanbrwycoff@gmail.com>
2016-01-14 19:55:37 +0100

    Added imports to syse.py from __init__.py

--- syse/__init__.py
+++ syse/__init__.py
@@ -1,8 +1,2 @@
-import pandas as pd
-from stat_parser import Parser
-import re
-import numpy as np
-import unicodedata
-import math
-from pkg_resources import resource_filename
+
 from .syse import SySE
--- syse/syse.py
+++ syse/syse.py
@@ -1,3 +1,11 @@
+import pandas as pd
+from stat_parser import Parser
+import re
+import numpy as np
+import unicodedata
+import math
+from pkg_resources import resource_filename
+
 ###
 #Syntactic Sentence Extraction (SySE)
 
